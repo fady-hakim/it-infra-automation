b476c926b92c1ff20b1968e82e29a4fd258c45c4
Fady <fady@Fadys-MacBook-Pro.local>
2026-06-26 04:00:17 +0100

    fix: correct Terraform variable syntax in vm-hardened module

--- terraform/modules/vm-hardened/variables.tf
+++ terraform/modules/vm-hardened/variables.tf
@@ -6,9 +6,25 @@ variable "name"                { type = string }
 variable "resource_group_name" { type = string }
 variable "location"            { type = string }
 variable "subnet_id"           { type = string }
-variable "vm_size"             { type = string  ; default = "Standard_B2s" }
 variable "admin_username"      { type = string }
-variable "admin_password"      { type = string  ; sensitive = true }
-variable "os_disk_size_gb"     { type = number  ; default = 64 }
 variable "storage_account_uri" { type = string }
-variable "tags"                { type = map(string) ; default = {} }
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B2s"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "os_disk_size_gb" {
+  type    = number
+  default = 64
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
